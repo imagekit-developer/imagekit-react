1d1c9c32d981b4dd8120e195d427fc238f221bb3
utkace <utkarsh@mindship.io>
2020-01-14 19:25:11 +0530

    chore: initial setup

---
+++ babel.config.js
@@ -0,0 +1,9 @@
+"use strict";
+
+module.exports = function (api){
+  api.cache.never();
+  return {
+    plugins: ["@babel/proposal-export-default-from","@babel/plugin-proposal-class-properties"],
+  "presets":
+    ["@babel/preset-env", "@babel/preset-react"],
+}};
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const { resolve } = require("path");
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'production',
+  output: {
+    path: resolve(process.cwd(), 'dist'),
+    filename: 'imagekit-react.js',
+    library: 'imageKitReact',
+    libraryTarget: 'umd',
+    publicPath: '/dist/',
+    umdNamedDefine: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  externals: {
+    "React": "react",
+    "imagekit": "imagekit-javascript"
+  }
+};
