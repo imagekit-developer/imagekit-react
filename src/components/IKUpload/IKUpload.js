4dddc808d2549ad0427d8bc77fd5c9d67987ce28
utkace <utkarsh@mindship.io>
2020-01-14 21:59:47 +0530

    feature: add ImageKitContext component

---
+++ src/components/IKUpload/IKUpload.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ImageKitComponent from "../ImageKitComponent/ImageKitComponent";
+import extractImageKitProps from '../../util/extractImageKitProps';
+
+export default class IKUpload extends ImageKitComponent {
+
+  onChangeHandler = (customOnChangeHandler, e, fileName, useUniqueFileName, tags, folder, isPrivateFile, customCoordinates, responseFields, extendedProps) => {
+    if (customOnChangeHandler) {
+      customOnChangeHandler(e);
+      return;
+    }
+
+    this.upload(e.target, e.target.files[0], fileName, useUniqueFileName, tags, folder, isPrivateFile, customCoordinates, responseFields, extendedProps)
+  }
+
+  render() {
+    const props = { ...this.props };
+    const { nonImageKitProps } = extractImageKitProps(props);
+    const context = this.getContext();
+    let extendedProps = ImageKitComponent.normalizeOptions(context, props);
+    let { fileName, useUniqueFileName, tags, folder, isPrivateFile, customCoordinates, responseFields } = this.props;
+    if(useUniqueFileName === true) useUniqueFileName = "true"
+    if(useUniqueFileName === false) useUniqueFileName = "false"
+    if(isPrivateFile === true) isPrivateFile = "true"
+    if(isPrivateFile === false) isPrivateFile = "false"
+    return (
+      <input {...nonImageKitProps} type="file" onChange={(e) => this.onChangeHandler(props.onChange, e, fileName, useUniqueFileName || "true", tags, folder || "/", isPrivateFile || "false", customCoordinates || "", responseFields || "", extendedProps)} />
+    )
+
+  }
+}
