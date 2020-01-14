47ce2fd9c2a6083c2acedb1d170c46fe5a222c3d
utkace <utkarsh@mindship.io>
2020-01-14 22:02:59 +0530

    helper: add helper function to extract props

---
+++ src/util/extractImageKitProps.js
@@ -0,0 +1,49 @@
+
+const IMAGEKIT_REACT_PROPS = {includeOwnBody : true, src:true, transformation:true};
+
+const IMAGEKIT_PROPS = {
+  publicKey: true,
+  urlEndpoint: true,
+  authenticationEndpoint: true,
+  lqip: true,
+  onError: true,
+  onSuccess: true,
+  fileName: true,
+  tags: true,
+  useUniqueFileName: true,
+  responseFields: true,
+  isPrivateFile: true,
+  folder: true,
+  customCoordinates: true
+};
+
+const isDefined = (props, key) => {
+  return (props[key] !== undefined && props[key] !== null);
+};
+
+export default ({children, ...props}) => {
+
+  let result = {
+    children,
+    imageKitProps: {},
+    nonImageKitProps: {},
+    imageKitReactProps: {}
+  };
+
+  Object.keys(props).forEach(key => {
+    const value = props[key];
+    if(IMAGEKIT_PROPS[key]) {
+      if(isDefined(props, key)) {
+        result.imageKitProps[key] = value;
+      }
+    }
+    else if(IMAGEKIT_REACT_PROPS[key]) {
+      result.imageKitReactProps[key] = value;
+    }
+    else {
+      result.nonImageKitProps[key] = value;
+    }
+  });
+
+  return result;
+};
