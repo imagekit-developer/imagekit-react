7262b931a76f39a8b5b65cca20cbf86b3e742846
utkace <utkarsh@mindship.io>
2020-01-14 21:54:24 +0530

    feature: add ImageKitImage component

---
+++ src/components/IKImage/IKImage.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import ImageKitComponent from "../ImageKitComponent/ImageKitComponent";
+import extractImageKitProps from '../../util/extractImageKitProps';
+
+export default class IKImage extends ImageKitComponent {
+  constructor(props, context) {
+    super(props, context);
+    this.imageRef = React.createRef();
+    this.state = {};
+
+    let state = { url: undefined };
+    this.state = Object.assign(state, this.prepareState(props, context));
+  }
+
+  prepareState(props = this.props, context = this.getContext()) {
+    let extendedProps = ImageKitComponent.normalizeOptions(context, props);
+    let url = this.getUrl(extendedProps);
+
+    let state = {};
+
+    let currentState = this.state || {};
+
+    if (url !== currentState.url) {
+      state.url = url;
+    }
+
+    return state;
+  }
+
+  lqipload(quality) {
+    let url = this.state.url;
+    let lqip = "";
+    if (this.props.path !== undefined) {
+      let newUrl = url.split("tr:");
+      if (newUrl[0] === url) {
+        let newUrl = url.split("/");
+        newUrl = `${newUrl[0]}//${newUrl[2]}/${newUrl[3]}/tr:q-${quality}/${newUrl[4]}`;
+        lqip = `${newUrl}`;
+      } else {
+        newUrl = `${newUrl[0]}tr:q-${quality}${newUrl[1]}`;
+        lqip = `${newUrl}`;
+      }
+    } else {
+      lqip = `${url}?tr=q-${quality}`;
+    }
+    return lqip;
+  }
+
+  componentDidMount() {
+    const imageObserver = new IntersectionObserver(function (entry, observer) {
+      if (entry[0].isIntersecting) {
+        let image = entry[0].target;
+        image.src = url;
+        imageObserver.unobserve(image);
+      }
+    });
+
+    const image = this.imageRef.current;
+    let url = this.state.url;
+    if (!image) return
+
+    imageObserver.observe(image);
+
+  }
+
+  componentDidUpdate() {
+    this.setState(this.prepareState());
+  }
+
+  render() {
+    let { url } = this.state;
+    const props = { ...this.props };
+    const { nonImageKitProps } = extractImageKitProps(props);
+    const lqip = props.lqip;
+
+    if (lqip !== undefined && lqip.active === true) {
+      const { quality } = this.props.lqip;
+      const url = this.lqipload(quality);
+      return < img src={url} {...nonImageKitProps} ref={this.imageRef} alt="" />;
+    } else {
+      return < img src={url} {...nonImageKitProps} ref={this.imageRef} alt="" />;
+    }
+  }
+}
+
+Image.propTypes = ImageKitComponent.propTypes;
