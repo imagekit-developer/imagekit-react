b9736043942fde90841285a3a88a561aef8c85d4
utkace <utkarshm27@hotmail.com>
2020-03-05 18:08:16 +0530

    functionality: handle onError and onSuccess

--- src/components/IKUpload/IKUpload.js
+++ src/components/IKUpload/IKUpload.js
@@ -4,13 +4,13 @@ import extractImageKitProps from '../../util/extractImageKitProps';
 
 export default class IKUpload extends ImageKitComponent {
 
-  onChangeHandler = (customOnChangeHandler, e, fileName, useUniqueFileName, tags, folder, isPrivateFile, customCoordinates, responseFields, extendedProps) => {
+  onChangeHandler = (customOnChangeHandler, e, fileName, useUniqueFileName, tags, folder, isPrivateFile, customCoordinates, responseFields, extendedProps,  onError, onSuccess) => {
     if (customOnChangeHandler) {
       customOnChangeHandler(e);
       return;
     }
 
-    this.upload(e.target, e.target.files[0], fileName, useUniqueFileName, tags, folder, isPrivateFile, customCoordinates, responseFields, extendedProps)
+    this.upload(e.target.files[0], fileName, useUniqueFileName, tags, folder, isPrivateFile, customCoordinates, responseFields, extendedProps, onError, onSuccess)
   }
 
   render() {
@@ -18,13 +18,13 @@ export default class IKUpload extends ImageKitComponent {
     const { nonImageKitProps } = extractImageKitProps(props);
     const context = this.getContext();
     let extendedProps = ImageKitComponent.normalizeOptions(context, props);
-    let { fileName, useUniqueFileName, tags, folder, isPrivateFile, customCoordinates, responseFields } = this.props;
+    let { fileName, useUniqueFileName, tags, folder, isPrivateFile, customCoordinates, responseFields, onError, onSuccess } = this.props;
     if(useUniqueFileName === true) useUniqueFileName = "true"
     if(useUniqueFileName === false) useUniqueFileName = "false"
     if(isPrivateFile === true) isPrivateFile = "true"
     if(isPrivateFile === false) isPrivateFile = "false"
     return (
-      <input {...nonImageKitProps} type="file" onChange={(e) => this.onChangeHandler(props.onChange, e, fileName, useUniqueFileName || "true", tags, folder || "/", isPrivateFile || "false", customCoordinates || "", responseFields || "", extendedProps)} />
+      <input {...nonImageKitProps} type="file" onChange={(e) => this.onChangeHandler(props.onChange, e, fileName, useUniqueFileName || "true", tags, folder || "/", isPrivateFile || "false", customCoordinates || "", responseFields || "", extendedProps,  onError, onSuccess)} />
     )
 
   }
