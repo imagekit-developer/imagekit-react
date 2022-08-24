54cb8202af437fefdca453a4d56d6b32fb25d4a6
Vishal <vishal@Vishals-MacBook-Pro.local>
2022-08-24 12:27:40 +0530

    Cypress-config.ts file added in cypress folder

---
+++ tests/cypress/cypress.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  fixturesFolder: 'fixtures',
+  screenshotsFolder: 'screenshots',
+  videosFolder: 'videos',
+  env: {
+    APP_HOST: 'http://localhost:3000/',
+  },
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./plugins/index.js')(on, config)
+    },
+    specPattern: 'e2e/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: 'support/index.js',
+  },
+})
