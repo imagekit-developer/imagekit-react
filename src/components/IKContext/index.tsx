d4a1b1842d67443c77d81f56ddf3e6938774b617
Nikhil Sejwal <nikhilsejwal@Nikhils-MacBook-Air.local>
2023-08-01 10:47:09 +0530

    feat: imagekitcomponent to functional custom hook

--- src/components/IKContext/index.tsx
+++ src/components/IKContext/index.tsx
@@ -58,3 +58,61 @@ class IKContext extends ImageKitComponent<IKContextCombinedProps> {
 
 
 export default IKContext;
+
+// import React, { createContext, useContext } from 'react';
+// import ImageKitComponent from "../ImageKitComponent";
+// import { InferProps } from 'prop-types';
+// import ImageKit from 'imagekit-javascript';
+// import { IKContextCombinedProps } from "./props";
+
+// // Create the context
+// const ImageKitContext = createContext<IKContextCombinedProps>({});
+
+// /**
+//  * Provides a container for ImageKit components. Any option set in IKContext will be passed to the children.
+//  *
+//  * @example
+//  *<IKContext  publicKey="<public key>" urlEndpoint="url link">
+//  *    <!-- other tags -->
+//  *    <Image src={link}/>
+//  *</IKContext>
+//  */
+// function IKContext(props: React.PropsWithChildren<IKContextCombinedProps>) {
+//   const contextOptions = useContext(ImageKitContext);
+
+//   const extractContextOptions = (mergedOptions: InferProps<IKContextCombinedProps>) => {
+//     var result: IKContextCombinedProps = {};
+
+//     const propKeys = Object.keys(IKContextCombinedProps);
+
+//     for (var i = 0; i < propKeys.length; i++) {
+//       var key = propKeys[i];
+//       const value = mergedOptions[key as keyof IKContextCombinedProps];
+//       if (value) {
+//         result[key as keyof IKContextCombinedProps] = value;
+//       }
+//     }
+
+//     return result;
+//   };
+
+//   const mergedOptions = { ...contextOptions, ...props };
+
+//   const contextOptionsExtracted = extractContextOptions(mergedOptions);
+
+//   if (contextOptionsExtracted.urlEndpoint && contextOptionsExtracted.urlEndpoint.trim() !== "") {
+//     contextOptionsExtracted.ikClient = new ImageKit({
+//       urlEndpoint: contextOptionsExtracted.urlEndpoint,
+//       // @ts-ignore
+//       sdkVersion: "",
+//     });
+//   }
+
+//   return (
+//     <ImageKitContext.Provider value={contextOptionsExtracted}>
+//       {props.children}
+//     </ImageKitContext.Provider>
+//   );
+// }
+
+// export default IKContext;
