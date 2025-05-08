f91a89b79325ba18b201e01c8b029d59e65cfc11
Manu Chaudhary <manu@imagekit.io>
2025-05-08 11:19:04 +0530

    Refactor SSR configuration in react-router to maintain server-side rendering

--- test-app/react-router.config.ts
+++ test-app/react-router.config.ts
@@ -2,6 +2,5 @@ import type { Config } from "@react-router/dev/config";
 
 export default {
   // Config options...
-  ssr: true, // Since we cannot disable ssr on selected pages, setting it to false here and using prerender for one path specifically
-  prerender: ["/ssr"], // This will essentially prerender and hamari ssr ki testing ho jayegi
+  ssr: true,
 } satisfies Config;
